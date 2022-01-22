c01fad1e7d5e9c61e907f3c51ffc3e087ed8c18e
hamitmizrak <hamitmizrak@gmail.com>
2022-01-22 18:04:09 +0300

    Kısa yollar

---
+++ src/main/java/com/patika/tutorials/ShortCuts.java
@@ -0,0 +1,28 @@
+package com.patika.tutorials;
+
+public class ShortCuts {
+
+    //Shift+F6 =Refactor
+    //Alt+Insert = getter setter,
+    //Ctrl+Shidt+o = maven install
+    //Ctrl+F2= terminate
+    //Ctrl+Alt+l =format
+
+    //Insert pasif aktif
+
+//    Settings -> Editor -> Code Style -> Inspections -> Proofreading -> Typo (Yazım hatalarının altını çizerek belirtme/belirtmeme)
+//    Alt + Insert --> Getter, Setter, toString etc. Kısa Yol Menüsü
+//    Ctrl + Shift + S --> Power Save Mode
+//    Shift + F6 --> Refactor (Rename)
+//    Ctrl + Shift + F10 --> Run Project
+//    Ctrl + Shift + F9 --> Build Project
+//    Ctrl + Shift + F2 --> Terminate (Stop) Project
+//    Ctrl + Shift + O --> Maven Install
+//    Ctrl + Shift + K --> Git Push
+//    Shift + K --> Git Commit
+//    Ctrl+/ = single commend
+//    Ctrl+Shift+/=mutliple commend
+//   Ctrl+Shift+ (-) ==> collapse modu
+    // Ctrl+D= o satırı kopyalar
+
+}
