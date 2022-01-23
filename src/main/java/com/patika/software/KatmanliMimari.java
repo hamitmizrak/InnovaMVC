b79368cd5a50d0b50bd8cb3e448f27977ea82743
hamitmizrak <hamitmizrak@gmail.com>
2022-01-23 16:42:16 +0300

    KatmanliMimari

---
+++ src/main/java/com/patika/software/KatmanliMimari.java
@@ -0,0 +1,10 @@
+package com.patika.software;
+
+public class KatmanliMimari {
+    //Katmanlı mimari projelerimizi belirli bir standart ve düzene göre geliştirmemizi sağlayan, kodun okunabilirliğini arttıran, projelerimizin daha derli toplu olmasını sağlayan ve hata yönetimini daha kolay hale getiren bir yapıdır.
+    //uygulamanın geliştirilmesi kolay olması açısından yapılan her işin ayrı bir katmanda gerçekleştirilmesi
+    //projemizi gelecege yonelik kodlayabilmek icin parcalara ayirarak yonetimini kolaylastirmak
+
+
+
+}
