116a307f2ac6729e98fecf6bf922c55caef433eb
hamitmizrak <hamitmizrak@gmail.com>
2022-01-23 16:40:06 +0300

    Dummy Codes

---
+++ src/main/java/com/patika/software/DummyCodes.java
@@ -0,0 +1,7 @@
+package com.patika.software;
+
+public class DummyCodes {
+
+    //dummy codes: kullanılmayan veya kullanıolmayacak kodları  UI entegre edip boş yerine Ram v.b sistemi yormaya denir.
+
+}
