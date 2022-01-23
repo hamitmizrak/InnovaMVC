06c70e5f388d425990e71427904a2634a9e51d89
hamitmizrak <hamitmizrak@gmail.com>
2022-01-23 16:46:18 +0300

    DRY

---
+++ src/main/java/com/patika/software/DRY.java
@@ -0,0 +1,6 @@
+package com.patika.software;
+
+public class DRY {
+    //Dont Repeay YourSelf :kendine tekrarlama
+    //Method:  yani bir kodu bir yerde yazıp farklı yerlerde de kullanmak gerekir
+}
