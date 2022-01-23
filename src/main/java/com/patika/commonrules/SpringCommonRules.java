1b476e1aabaf8901abca701e376656bae1659fbe
hamitmizrak <hamitmizrak@gmail.com>
2022-01-23 17:16:43 +0300

    SpringCommonRules JSR (Java Specification Requests) Sartname JSR 330(Dependency Injection) gibi şartnameleri kullanır. JSR 369(Servlet) , JSR 380(Bean Validation) ,

---
+++ src/main/java/com/patika/commonrules/SpringCommonRules.java
@@ -0,0 +1,20 @@
+package com.patika.commonrules;
+
+public class SpringCommonRules {
+// path:
+// türkçe karakter yazmalım: üğşçö
+// boşluk %20%
+// en üst dizinde yazmak  c:\\spring
+// Jsp,Jsf ==> War
+
+//    JSR (Java Specification Requests) Sartname
+//    JSR 330(Dependency Injection) gibi şartnameleri kullanır.
+//    JSR 369(Servlet) ,
+//    JSR 380(Bean Validation) ,
+
+
+
+
+
+
+}
