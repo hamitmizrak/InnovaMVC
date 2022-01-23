5ee0f067ce2c4379a0a963aabc526ae73ceb2aa2
hamitmizrak <hamitmizrak@gmail.com>
2022-01-23 16:44:41 +0300

    CCP

---
+++ src/main/java/com/patika/software/CcpPrensible.java
@@ -0,0 +1,7 @@
+package com.patika.software;
+
+public class CcpPrensible {
+   // Common Closure Principle (CCP) – Ortak Kapama Prensibi
+  //  CCP’ye göre yapılan değişikliklerin sistemin büyük bir bölümünü etkilemesini önlemek için, aynı sebepten dolayı değişikliğe uğrayabilecek sınıfların aynı paket içinde yer alması gerekir.
+  //  CCP uygulandığı taktirde sistemin bakılabilirliği artırılır ve test ve yeni sürüm için harcanan zaman ve emek azaltılır.
+}
