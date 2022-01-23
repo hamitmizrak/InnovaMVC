c50c7e530231a5ffc185e0ab0b14f1613a7ca67b
hamitmizrak <hamitmizrak@gmail.com>
2022-01-23 16:29:34 +0300

    TernaryString

---
+++ src/main/java/com/patika/tutorials/TernaryString.java
@@ -0,0 +1,12 @@
+package com.patika.tutorials;
+
+public class TernaryString {
+
+    public static void main(String[] args) {
+        int sayi=5;
+        String ternary= (sayi==5) ?"sayı 5"  :"5 değil" ;
+        System.out.println(ternary);
+    }
+
+
+}
