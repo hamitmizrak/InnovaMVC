f76edb5ce5e15763f5c60e0cbf9ea3f04872cca1
hamitmizrak <hamitmizrak@gmail.com>
2022-01-23 16:34:28 +0300

    SpagettiCodes

---
+++ src/main/java/com/patika/software/SpagettiCodes.java
@@ -0,0 +1,9 @@
+package com.patika.software;
+
+public class SpagettiCodes {
+    //main bloğunda metotlar classlar
+    //main bloğunda metotlar classlar
+    //karmaşık içiçe geçmiş kodlar
+    //gelişi güzel yazaılan, iç içe girmiş değşiklik yapmak zor olan kod parçaları
+    //if elselerin cok oldugu ve herseyin iç içe yazıldıgı kod
+}
