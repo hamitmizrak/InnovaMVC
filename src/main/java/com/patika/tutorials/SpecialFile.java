37821417a647ffd132b1d2967ec5feeefa76cdee
hamitmizrak <hamitmizrak@gmail.com>
2022-02-05 16:10:57 +0300

    SpecialFile

--- src/main/java/com/patika/tutorials/SpecialFile.java
+++ src/main/java/com/patika/tutorials/SpecialFile.java
@@ -7,10 +7,10 @@ import java.nio.file.Paths;
 public class SpecialFile {
 
     public static void specialSecretMethod(){
-        String special=new String(Files.readAllBytes(Paths.get("special.txt"), Charset.forName("utf-8")));
+       // String special=new String(Files.readAllBytes(Paths.get("special.txt"), Charset.forName("utf-8")));
         //database gönder
 
-        byte [] resimVideo=Files.readAllBytes(Paths.get("resim.png"));
+       // byte [] resimVideo=Files.readAllBytes(Paths.get("resim.png"));
 
     }
 
