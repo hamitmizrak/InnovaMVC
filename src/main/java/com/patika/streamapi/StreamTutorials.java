c9ddfb762ed27d8e9a7fc51f37d9c6f0477cbcf5
hamitmizrak <hamitmizrak@gmail.com>
2022-02-05 16:24:22 +0300

    stream()

---
+++ src/main/java/com/patika/streamapi/StreamTutorials.java
@@ -0,0 +1,38 @@
+package com.patika.streamapi;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamTutorials {
+
+    //Collection ==> List,Set,Map
+    //Stream API
+    //Optional
+    //Lombok
+    //lambda expression
+
+
+
+    public static void main(String[] args) {
+        List<String> list=new ArrayList<>();
+        list.add("malatya");
+        list.add("istanbul");
+        list.add("ankara");
+        List<String> list2=list.stream().sorted().collect(Collectors.toList());
+        list2.forEach(System.out::println);
+
+
+        List<String> list3=list.stream().sorted().collect(Collectors.toList());
+        list2.forEach(System.out::println);
+
+        list.stream().filter( patika ->!"malatya".equals(patika)).collect(Collectors.toList());
+
+
+        System.out.println("++++++++++++++++++++++++++++");
+        List<String> list5= Arrays.asList("malatya","istanbul","ankara");
+
+    }
+
+}
